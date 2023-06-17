66cc9f7c4023ed622a606e20bfe72300fa859348
JudCode <root@Juduntu.mrjud.ubuntu.org>
2023-06-18 02:52:52 +1000

    Fixes to deploy 8

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,7 +1,7 @@
 export default function NavBar({ navBarColor, navBarBgColor, navBarTitleBorderColor } : { navBarColor : string, navBarBgColor : string, navBarTitleBorderColor : string }) {
 	return (
 		<section className={`${navBarColor} ${navBarBgColor} w-full absolute px-4 z-20`}>
-			<div className={'container flex justify-between items-center mx-auto'}>
+			<div className={'container flex justify-between items-center mx-auto max-w-6xl'}>
 				<a href="menu" className={'block font-hauora uppercase'}>Menu</a>
 				<a href="gallery" className={'block font-hauora uppercase'}>Gallery</a>
 				<a href="/aleph-coffee-van-next-js" className={`block text-center ${navBarTitleBorderColor}`}>
